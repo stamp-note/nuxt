31e4ccb14d564f678bbe2d97c4bb8a5eea6d24a7
이윤건(Macko) <sangok1993@naver.com>
2023-07-17 15:09:34 +0900

    test

---
+++ components/containers/nuxt/DecreaseBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-btn
+        @click="decrease"
+        color="primary"
+    >
+      +
+    </v-btn>
+  </div>
+</template>
+<script>
+import { useCounterStore} from "~/stores/counter";
+
+export default {
+  setup() {
+    const store = useCounterStore();
+    return {
+      store,
+    }
+  },
+  methods: {
+    decrease() {
+      this.store.decrease();
+    },
+  },
+}
+</script>
---
+++ components/containers/nuxt/IncreaseBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-btn
+        @click="increment"
+        color="primary"
+    >
+      +
+    </v-btn>
+  </div>
+</template>
+<script>
+import { useCounterStore} from "~/stores/counter";
+
+export default {
+  setup() {
+    const store = useCounterStore();
+    return {
+      store,
+    }
+  },
+  methods: {
+    increment() {
+      this.store.increment();
+    },
+  },
+}
+</script>
