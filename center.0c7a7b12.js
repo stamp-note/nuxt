92ea10660cbb29ce9306c5298a2a2b2dd4a2a68b
MackoLee <sangok1993@naver.com>
2023-08-20 16:36:02 +0000

    Deploying to gh-pages from @ stamp-note/nuxt@ffc95ce0da648f5e8eab7ffbda95f6927e08fa6d 🚀

---
+++ center.0c7a7b12.js
@@ -0,0 +1,1 @@
+import{_ as s,c as t,a as o,k as a,o as c}from"./entry.94f8a2e2.js";const r={},n={class:"d-flex w-100 h-100"},_={class:"ma-auto"};function d(e,l){return c(),t("div",n,[o("div",_,[a(e.$slots,"default")])])}const i=s(r,[["render",d]]);export{i as default};
