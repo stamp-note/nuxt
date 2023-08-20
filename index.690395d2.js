c2fe245d15b06ecc6edd2900b420bbdd314a49df
MackoLee <sangok1993@naver.com>
2023-08-20 17:02:22 +0000

    Deploying to gh-pages from @ stamp-note/nuxt@af196bd818d83e1f40a71e3bfd3215b29cfca103 🚀

--- index.690395d2.js
+++
@@ -1,1 +0,0 @@
-import{f as n,_ as o,c as s,t as c,o as r}from"./entry.42847024.js";const u=n("counter",{state:()=>({count:0}),actions:{increment(){this.count++},decrease(){this.count--}}}),a={setup(){return{store:u()}},computed:{count(){return this.store.count}}};function i(t,p,d,_,f,e){return r(),s("div",null," nuxt "+c(e.count),1)}const m=o(a,[["render",i]]);export{m as default};
