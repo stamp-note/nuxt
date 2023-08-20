9580e74d3119e0c2473ac7e61f978884733b8689
이윤건(Macko) <sangok1993@naver.com>
2023-08-21 02:04:55 +0900

    Update store.ts

--- components/NuxtPage/store.ts
+++ components/NuxtPage/store.ts
@@ -24,10 +24,10 @@ export const useMainStore = defineStore({
             this.saveViewMode();
         },
         loadViewMode() {
-            this.viewMode = localStorage.getItem('viewMode') || 'Customer & Admin';
+            this.viewMode = window.localStorage.getItem('viewMode') || 'Customer & Admin';
         },
         saveViewMode() {
-            localStorage.setItem('viewMode', this.viewMode);
+            window.localStorage.setItem('viewMode', this.viewMode);
         }
     },
 })
